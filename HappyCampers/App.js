99880856976f303e5c8c78122bf9439799d394dd
Vicki Wong <vicki.wong85@gmail.com>
2023-12-14 23:52:21 -0800

    add splash screen and landing page

--- HappyCampers/App.js
+++ HappyCampers/App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Button, Text, View, Image } from 'react-native';
+import SplashScreen from './SplashScreen.js';
 
 export default function App() {
+  const [renderSplashScreen, setRenderSplashScreen] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setRenderSplashScreen(false), 3000);
+  }, [])
 
   return (
-    <View style={styles.container}>
-      <Text>Happy Campers</Text>
+    renderSplashScreen ? <SplashScreen /> : (<View style={styles.container}>
+      <View style={styles.button}>
+        <Image source={require('./assets/map.jpg')}></Image>
+        <Button title="Search Campsite" />
+      </View>
+      <View style={styles.button}>
+        <Image source={require('./assets/tent.jpg')}></Image>
+        <Button title="My Reservations" />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </View>)
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'white',
+    borderColor: 'white',
+    borderRadius: 30,
+    borderWidth: 2,
+    height: 300,
+    justifyContent: 'center',
+    padding: '3%',
+    margin: '4%'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d22e2e',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ HappyCampers/SplashScreen.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const SplashScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('./assets/happycampers.jpg')}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    flex: 1,
+    width: '80%',
+    height: '80%',
+    resizeMode: 'contain'
+  },
+});
+
+export default SplashScreen;
