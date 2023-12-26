cf500c7eb155bca2ea613291f486c194952e71df
Vicki Wong <vicki.wong85@gmail.com>
2023-12-26 15:52:22 -0800

    add recreation api and search form

--- HappyCampers/components/Map.js
+++ HappyCampers/components/Map.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Map = () => {
   return (
     <View style={styles.container}>
       <MapView style={styles.map} />
+      <Text>Data source: ridb.recreation.gov</Text>
     </View>
   )
 }
@@ -13,10 +14,11 @@ const Map = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center'
   },
   map: {
-    width: '50%',
-    height: '50%',
+    width: '100%',
+    height: '70%',
   }
 });
 
--- HappyCampers/components/SearchForm.js
+++ HappyCampers/components/SearchForm.js
@@ -1,13 +1,94 @@
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 const SearchForm = ({ navigation }) => {
+  const [text, onChangeText] = useState('');
+
+  function searchRecreationAreas(queryString) {
+    console.log(queryString)
+    axios.get('https://ridb.recreation.gov/api/v1/recareas', {
+      params: {
+        apikey: process.env.APIKEY,
+        accept: 'application/json',
+        query: queryString,
+        limit: 2,
+        offset: 0,
+      }
+    })
+      .then((result) => {
+        console.log(result.data.RECDATA[1].RecAreaID);
+        const recAreaId = result.data.RECDATA[1].RecAreaID
+        axios.get(`https://ridb.recreation.gov/api/v1/recareas/${recAreaId}/facilities`, {
+          params: {
+            apikey: process.env.APIKEY,
+            accept: 'application/json',
+            limit: 1
+          }
+        })
+          .then((result) => {
+            console.log(result.data.RECDATA[0].FacilityID);
+            const facilityID = result.data.RECDATA[0].FacilityID;
+            axios.get(`https://ridb.recreation.gov/api/v1/facilities/${facilityID}/campsites`, {
+              params: {
+                apikey: process.env.APIKEY,
+                limit: 1
+              }
+            })
+              .then((result) => {
+                console.log(result.data.RECDATA[0].ENTITYMEDIA[0].Title);
+                console.log(result.data.RECDATA[0].ENTITYMEDIA[0].URL);
+                axios.get(`https://ridb.recreation.gov/api/v1/facilities/${facilityID}/links`, {
+                  params: {
+                    apikey: process.env.APIKEY,
+                    facilityID
+                  }
+                })
+                  .then((result) => {
+                    console.log(result.data.RECDATA[0].URL);
+                  })
+                  .catch((err) => {
+                    console.log({err})
+                  })
+              })
+              .catch((err) => {
+                console.log({err})
+              })
+          })
+          .catch((err) => {
+            console.log({err})
+          })
+      })
+      .catch((err) => {
+        console.log({err})
+      })
+  }
+
+  function handleSearch() {
+    searchRecreationAreas(text);
+    navigation.navigate('Map');
+  }
+
   return (
     <View>
-      <Text>Search for a campsite: </Text>
-      <Button title="Go!" onPress={() => navigation.navigate('Map')}/>
+      <Text style={styles.label}>Search by name of location or area: </Text>
+      <TextInput style={styles.input} value={text} onChangeText={onChangeText}/>
+      <Button title="Go!" onPress={() => handleSearch()}/>
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  label : {
+    padding: 15,
+    fontSize: 15,
+  }
+});
 
 export default SearchForm;
