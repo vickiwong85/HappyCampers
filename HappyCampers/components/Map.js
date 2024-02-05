ef4d305e69bc7e966521898bb8ef5433c375bb9a
Vicki Wong <vicki.wong85@gmail.com>
2024-02-04 16:49:47 -0800

    add results panel

--- HappyCampers/components/Map.js
+++ HappyCampers/components/Map.js
@@ -1,35 +1,57 @@
 import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 const Map = ({ route, navigation }) => {
 
   const { markers } = route.params;
 
-  console.log({markers})
+  const [ isPressed, setIsPressed ] = React.useState(false);
+
+  var touchProps = {
+    activeOpacity: 0.2,
+    underlayColor: 'light gray',
+    style: styles.resultItem,
+    onHideUnderlay: () => setIsPressed(false),
+    onShowUnderlay: () => setIsPressed(true),
+    onPress: () => console.log('isPressed'),
+  };
+
+  console.log('in Map', {markers})
 
   const initialRegion = {
-    latitude: 37.8272042070001,
-    longitude: -119.596809489,
+    latitude: 37.7513572400001,
+    longitude: -119.735838815,
     latitudeDelta: 0.01,
     longitudeDelta: 0.01,
   };
 
   return (
     <View style={styles.container}>
-      {markers.length && <MapView
+      {<MapView
         style={styles.map}
         initialRegion={initialRegion}
         >
-        {markers.map((marker) =>
-          <Marker key={marker.CampsiteID} coordinate={{
+        {markers.map((marker) => {
+          return (<Marker key={marker.id} coordinate={{
             latitude: marker.latitude,
             longitude: marker.longitude,
             latitudeDelta: marker.latitudeDelta,
             longitudeDelta: marker.longitudeDelta,
-          }}/>
+          }}/>)}
         )}
       </MapView>}
+      <View style={styles.resultsPanel}>
+      {markers.map((marker,i) => {
+        return (
+          <TouchableHighlight key={marker.title}  {...touchProps}>
+          <View>
+          <Text>{`${i + 1}. ${marker.title}`}</Text>
+          </View>
+          </TouchableHighlight>
+        )
+      })}
+      </View>
       <Text>Data source: ridb.recreation.gov</Text>
     </View>
   )
@@ -43,6 +65,20 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '50%',
+  },
+  resultItem: {
+    borderBottomColor: 'gray',
+    borderBottomWidth: 1,
+    paddingBottom: 10,
+  },
+  resultsPanel: {
+    borderColor: 'gray',
+    borderRadius: 30,
+    borderWidth: 2,
+    height: 300,
+    width: 350,
+    padding: '5%',
+    margin: '4%',
   }
 });
 
